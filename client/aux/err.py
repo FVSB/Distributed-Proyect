1a6184c3661187f49ec2196c40cfa834c754c6e7
MaykolLuisMa <maykoluis0@gmail.com>
2024-09-28 18:13:20 -0400

    fix join

--- client/aux/err.py
+++ client/aux/err.py
@@ -214,60 +214,7 @@ class ChordNode:
         """
         return self._key_range
 
-    def search_test(self):
-
-        while True:
-            try:
-                time.sleep(3)
-                log_message("%" * 20, level="INFO")
-                # with ThreadPoolExecutor(max_workers=3) as executor:
-                #    for i in range(0,20):
-                #        future=executor.submit(self.find_succ,i) # Meterlo en el pool de hilos
-                #        node=future.result(timeout=10)
-                #
-                #        log_message(f'El nodo que le pertenece el id {i} es el nodo con id {node.id}')
-                for i in range(0, 20):
-                    node = self.find_succ(i)
-                    log_message(
-                        f"El nodo que le pertenece el id {i} es el nodo con id {node.id}"
-                    )
-
-                log_message("+" * 20, level="INFO")
-            except Exception as e:
-                log_message(f"Error buscando informacion {e}", self.search_test)
-
-    def show(self, time_: int = 3):
-        """
-        Show my ip and id and mi predecessor and succesors ips and ids
-        """
-        while True:
-            log_message("-" * 20, level="INFO")
-            log_message(
-                f"Mi predecesor es {self.pred.id if self.pred else None} con ip {self.pred.ip if self.pred else None} ",
-                level="INFO",
-            )
-            log_message(f"Yo soy id:{self.id},con ip:{self.ip} ", level="INFO")
-            if self.succ.id == self.id:
-                if self.succ.ip != self.ip:
-                    log_message(
-                        "El sucesor tiene igual ID pero no tiene igual ip", level="INFO"
-                    )
-                log_message(f"Todavia no tengo sucesor", level="INFO"),
-            else:
-                log_message(
-                    f"Mi sucesor es {self.succ.id if self.succ else None} con ip {self.succ.ip  if self.succ else None}",
-                    level="INFO",
-                )
-
-            log_message(
-                f"La finger table es {self.finger} y está en estado {self.fingers_ok}",
-                func=self.show,
-            )
-
-            log_message("*" * 20, level="INFO")
-
-            time.sleep(time_)  # Se presenta cada 10 segundos
-
+    
     def _search_successor(self, op: int, data: str = None, time_: float = 0.5) -> bytes:
         """Busca un sucesor si no tengo o si mi pred es None
 
